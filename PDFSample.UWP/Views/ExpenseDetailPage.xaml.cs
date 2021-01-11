a27f4e1d5fe335bbdec3c0637d46954b084a8eca
Matteo Pagani <matteo.pagani@microsoft.com>
2021-01-11 13:09:16 +0100

    Add project files.

---
+++ PDFSample.UWP/Views/ExpenseDetailPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using PDFSample.UWP.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace PDFSample.UWP.Views
+{
+    public sealed partial class ExpenseDetailPage : Page
+    {
+        private ExpenseDetailViewModel ViewModel => DataContext as ExpenseDetailViewModel;
+
+        public ExpenseDetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PDFSample.UWP/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using PDFSample.UWP.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace PDFSample.UWP.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        private MainViewModel ViewModel => DataContext as MainViewModel;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
